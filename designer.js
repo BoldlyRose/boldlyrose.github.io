f8ea4a55db97e510815f301a66aca3d57b74888c
BoldlyRose <roseboldly@gmail.com>
2024-03-23 21:36:32 +0100

    implement make elements draggable function

--- designer.js
+++ designer.js
@@ -11,6 +11,36 @@ document.getElementById('addText').addEventListener('click', function () {
 });
 
 function makeElementDraggable(element) {
-    // Implement drag-and-drop functionality for the elements
-    // This involves listening for mouse events and updating the element's position accordingly
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    element.onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // Call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // Set the element's new position:
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // Stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
